5d074cac2db9a69a7d1ade279f92c18d8fa7abbb
patilprem21 <pprem2802@gmail.com>
2024-09-03 12:16:12 +0530

    Update for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -10,16 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
 const tasksRouter = require('./routes/tasks');
-
-app.use('/tasks', tasksRouter);
+app.use('/api/tasks', tasksRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
